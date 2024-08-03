ee0fad9121645bc50ff486594c4bedad4ed04dce
leonard <luclenny1@hotmail.com>
2024-08-03 07:31:17 -0500

    8/3 update Day 6

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public or private reference(if a person can see it, its public. if they cant, its private)
+    //the data type(int = whole numbers, floats = decimal numbers, bool = true or false, string = characters of text)
+    //every variable has a name
+    //optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    public float horizontalInput;
+
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        //new Vector3(0, 0, 0) * 0 * 3.5f * real time
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+       
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }            
+                      
+        if (transform.position.x >= 11.3)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -11.3)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, transform.position.z);
+        }
+        
+    }
+}
