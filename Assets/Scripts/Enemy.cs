f713efa48b0b0eff4eeaa80bfc814fd9c5ef3bb7
leonard <luclenny1@hotmail.com>
2024-08-03 21:02:55 -0500

    Added enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //move down at 4 meters per second        
+    //if bottom of screen
+    //respawn at top with a new random position
+    
+    [SerializeField]
+    private float _speed = 4.0f; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        float randomX = Random.Range(-8, 8);
+        if (transform.position.y < -5.5f)
+        {
+            transform.position = new Vector3(randomX, 7.5f, 0);
+        }
+    }
+}
